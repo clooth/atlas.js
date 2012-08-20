e3e089d6684dc4b10a96838572aefee0a0a98427
Nico Hämäläinen <nico@bender.fi>
2012-08-20 23:35:46 +0300

    Updated watchr script to include coffeescript and uglifyjs, added jcolour as an included file

--- watchr.rb
+++ watchr.rb
@@ -1,3 +1,17 @@
-watch( 'src/(.*)\.coffee' ) {
-    |md| puts "Updating docs..."; system("docco src/#{md[1]}.coffee && git add docs")
-}
+watch( 'src/(.*)\.coffee' ) do |md|
+    puts "Compiling coffeescript"
+    filelist = [
+        "src/jColour.coffee",
+        "src/util.coffee",
+        "src/canvas.coffee",
+        "src/atlas.coffee"
+    ]
+    filelistString = filelist.join(" ")
+    system("coffee -o . --join atlas.js --compile #{filelistString}")
+
+    puts "Uglifying javascript"
+    system("uglifyjs -o atlas.min.js atlas.js")
+
+    puts "Updating docs..."
+    system("docco src/#{md[1]}.coffee && git add docs")
+end
